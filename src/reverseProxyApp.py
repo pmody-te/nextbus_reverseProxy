7abdb08049dab7b304bd249c5ebc21d09b9546ae
Pranoy Mody <pranoymody@MacBook-Pro.local>
2017-12-03 19:25:34 -0500

    added execution time calculator for http requests and flask redis cache

--- src/reverseProxyApp.py
+++ src/reverseProxyApp.py
@@ -1,25 +1,41 @@
 import flask 
-from flask import Response
+from flask import Response,g
 import reverseProxy
 import tools 
 import logging 
 import time 
-
+from flask_caching import Cache
 
 APP = flask.Flask(__name__)
-config = tools.load_config("../config.ini")
+cache = Cache(APP, config={'CACHE_TYPE': 'redis','CACHE_REDIS_HOST': 'localhost','CACHE_REDIS_PORT':'6379','CACHE_DEFAULT_TIMEOUT':'5'})
+config = tools.load_config("/Users/pranoymody/Desktop/Code/reverseProxy/config.ini")
+
+
+@APP.before_request
+def before_request():
+	g.start = time.time()
 
 @APP.route('/api/v1/agencyList')
+@cache.cached()
 def agencyList():
 	url = reverseProxy.createURL(config,"agencyList")
 	xml = tools.http_request(url)
 	return Response(xml, mimetype='text/xml')
 
 @APP.route('/api/v1/routeList/<agency>/')
+@cache.cached()
 def routeList(agency):
 	url = reverseProxy.createURL(config,"routeList") + agency
 	xml = tools.http_request(url)
 	return Response(xml, mimetype='text/xml')
+
+@APP.after_request
+def after_request(response):
+    diff = time.time() - g.start
+    print "Exec time: %s" % str(diff)
+    if (response.response):
+        response.response[0] = response.response[0].replace('__EXECUTION_TIME__', str(diff))
+    return response
 
 if __name__ == '__main__':
     APP.debug=True
